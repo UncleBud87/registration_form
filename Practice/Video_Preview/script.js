3d8f4bdacfa6c9f16460916ff90917a5748b3972
UncleBud87 <lperioux@gmail.com>
2022-01-31 15:48:02 -0600

    commit_1

--- Practice/Video_Preview/script.js
+++ Practice/Video_Preview/script.js
@@ -1,1 +1,11 @@
-console.log("page loaded...");
+// console.log("page loaded...");
+
+function play(element) {
+    video.play(element); {
+        vid.muted = true;
+    }
+}
+
+function pause(element) {
+    video.pause(element);
+}
