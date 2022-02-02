125666dac2368d9e7f10a57590eb13e4cc423273
UncleBud87 <lperioux@gmail.com>
2022-02-01 19:01:02 -0600

    commit_1

--- Practice/Dojo_Weather/script.js
+++ Practice/Dojo_Weather/script.js
@@ -35,3 +35,24 @@ function temp(element) {
         h.innerText = "70°";
     }
 }
+
+// function c2f(temp) {
+//     return Math.round(9 / 5 * temp + 32);
+// }
+
+// function f2c(temp) {
+//     return Math.round(5 / 9 * (temp - 32));
+// }
+
+// function convert(element) {
+//     console.log(element.value);
+//     for(var i=1; i<9; i++) {
+//         var tempSpan = document.querySelector("#temp" + i);
+//         var tempVal = parseInt(tempSpan.innerText);
+//         if(element.value == "°C") {
+//             tempSpan.innerText = f2c(tempVal);
+//         } else {
+//             tempSpan.innerText = c2f(tempVal);
+//         }
+//     }
+// }
